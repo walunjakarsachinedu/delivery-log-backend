c5cfe6fc3fb49fe48aae8ccdce91e0e0b9e36d6a
Sachin Walunjakar <walunjakarsachin@gmail.com>
2026-04-10 21:40:44 +0530

    fixed db connection url

--- src/db/connect.ts
+++ src/db/connect.ts
@@ -9,7 +9,7 @@ export async function connectToDB(): Promise<void> {
     const password: string | undefined = getEnv("MONGODB_PASSWORD");
     const dbName: string | undefined = getEnv("MONGODB_DB_NAME");
     // clear();
-    const dbUrl =`mongodb+srv://${username}:${password}@cluster0.aqcdz0m.mongodb.net/?appName=Cluster0&retryWrites=true&w=majority`;
+    const dbUrl =`mongodb+srv://${username}:${password}@cluster0.aqcdz0m.mongodb.net/${dbName}?appName=Cluster0&retryWrites=true&w=majority`;
     // const dbUrl = `mongodb+srv://${username}:${password}@cluster0.zasrmm9.mongodb.net/${dbName}?retryWrites=true&w=majority`;
     await mongoose
       .connect(dbUrl)
